96eaaf5684166392f372fe6ce725a4fa14f6f1a3
Anshit_Agarwal <b23cs1087@iitj.ac.in>
2024-10-21 22:53:13 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function fetchData() {
+    // ESP8266 server IP address
+    const url = 'http://192.168.145.184/';
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Received data:', data);
+            document.getElementById('temperature').value = data.temperature + ' °C';
+            document.getElementById('humidity').value = data.humidity + ' %';
+            // Display soil moisture data
+            document.getElementById('soil-moisture').value = data.soilMoisture + ' %';
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+// Fetch data every 10 seconds
+setInterval(fetchData, 10000);
+
+// Initial fetch
+fetchData();
